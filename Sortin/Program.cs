8d5ef3cd5d8e2668e8dd7ba96b3895ab3f15aebc
janlaurenceestrada2100-hash <janlaurence.estrada2100@gmail.com>
2026-09-04 08:30:57 +0800

    Add project files.

---
+++ Sortin/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Sortin //Selection Sort
+{
+    class Program
+    {
+
+        // Main program
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = { 694, 295, 192, 292, 10, 11, 2, 191, 111, 444, 435 };
+                sort(arr);
+                Console.WriteLine("Sorted array");
+                printArray(arr);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error encountered. \n{e.ToString()}");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+        //Perform the selection sort algorithm
+        static void sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            // One by one move boundary of unsorted subarray
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Find the minimum element in unsorted array
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+
+                // Swap the found minimum element with the first
+                // element
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+                printArray(arr);
+            }
+        }
+        // Prints the array
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " | ");
+            Console.WriteLine();
+        }
+    }
+}
